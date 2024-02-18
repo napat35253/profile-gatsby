3c75eac4bcd7f1a3f611225ff921eb0e40231cd6
napat35253 <nbhahol@gmail.com>
2024-02-18 20:59:45 +0100

    add

--- gatsby-node.js
+++ gatsby-node.js
@@ -77,10 +77,10 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
             test: /scrollreveal/,
             use: loaders.null(),
           },
-          {
-            test: /animejs/,
-            use: loaders.null(),
-          },
+        //   {
+        //     test: /animejs/,
+        //     use: loaders.null(),
+        //   },
           {
             test: /miniraf/,
             use: loaders.null(),
